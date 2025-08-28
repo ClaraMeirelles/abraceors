a1d3f0d6af3d429e076c90db63b7a56805cd16b8
ClaraMeirelles <clara_meirelles@hotmail.com>
2025-08-28 09:51:53 -0300

    alteração no sobre

--- abraceors/src/Pages/AboutUs/AboutUs.js
+++ abraceors/src/Pages/AboutUs/AboutUs.js
@@ -7,6 +7,37 @@ export default function AboutUs() {
     return (
         <div>
             <Header />
+            {/* Ana Paula Brasil
+
+
+Ana Paula Brasil é artista visual, autora e designer, profissional internacional formada em artes plásticas e fotografia. Expertise, criatividade e conhecimento, combinados com as suas técnicas avançadas, são uma mistura explosiva única apresentada em seu Universo Artístico. Trabalha com artes têxteis para expressar a felicidade de viver.
+Ana nasceu em uma família de poetas, artistas e costureiras. Seu caso de amor com o Quilt começou aos 8 anos de idade com a sua avó. Desde os anos 90 ministra workshops e participa de programas de TV e seu trabalho tem aparecido em centenas de revistas pelo mundo, além de participar de exposições e feiras internacionais desde 2009. Seu trabalho de arte pode ser visto em exposições internacionais, museus e galerias de arte. Morando no Canadá, exibe sua arte compartilhando seu conhecimento artístico em sua linda e feliz jornada através do Art Quilt. Após sua estreia em Houston, destacou-se no cenário internacional com sua criatividade e estilo único.
+Ana Paula é proprietária de uma loja, atelier e escola de arte têxtil e quilt em Thunder Bay, Canadá. 
+Ana é sempre cheia de humor, com alto astral, histórias e dicas, garantindo que as aulas sejam técnicas e divertidas. Preocupada com o planeta, compartilha sua filosofia de lixo zero com todos e está sempre engajada em projetos sociais com o objetivo de levar amor e conhecimento. Acredita que não existem erros, mas sim oportunidades.
+Prepare-se para pensar fora da caixa e como ela diz: #vemnaminhaquetubrilha
+
+
+c
+
+
+✂️email contato
+brasilanapaula@me.com
+
+✂️Página da Artista 
+Ana Paula Brasil - https://www.facebook.com/AnaPaulaBrasill/
+cANAda pAULA brasil - https://www.facebook.com/cANadaPaulaBrasil/ 
+
+✂️Instagram da Artista 
+https://www.instagram.com/canadapaulabrasil/
+
+✂️Website da Artista
+www.canadapaulabrasil.ca 
+
+✂️YouTube da Artista 
+https://www.youtube.com/user/AnaPaulaBrasil
+
+✂️Link Tree
+https://linktr.ee/canadapaulabrasil */}
             <Section>
                 <img src={LogoBobina} alt="Bobina de máquina de costura desenhada a mão com escrito em efeito bordado o nome 'Bobina cheia de amor'" />
                 <h1>O que é o projeto "Bobina cheia de amor?"</h1>
